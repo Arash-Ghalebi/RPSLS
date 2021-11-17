f5b33a8d1e9feb954ea7d866d0454a5507fec92a
Arash Ghalebi <aghalebi33@gmail.com>
2021-11-17 11:11:04 -0500

    Formatting changes for console readability were made.

--- main.py
+++ main.py
@@ -6,34 +6,43 @@ import getpass
 print("----RPSLS RULES----")
 print("For rock: rock beats lizards, rock beats scissor. \nFor scissors: scissors beats paper, scissors beats lizard. \nFor paper: paper beats rock, paper beats Spock. \nFor lizard: lizard beats Spock, lizard beats paper. \nFor Spock: Spock beats scissors, Spock beats rock\n\n")
 
-numPlayers = int(input("How many players will be playing? 1 or 2?"))
+numPlayers = int(input("How many players will be playing? 1 or 2? "))
 player1_counter = 0
 player2_counter = 0
 player_1 =  Human(input("Input Player 1 name here: "))
 if numPlayers == 1:
     player_2 = Ai("Computer")
+    print('\n')
     while player1_counter < 2 and player2_counter < 2:
         print("1 for Rock, 2 for Scissors, 3 for Paper, 4 for Lizard, 5 for Spock")
         gesture1 = player_1.choose_gesture()
         gesture2 = player_2.choose_gesture()
         print(f'{player_1.name}s choice is: {gesture1}')
-        print(f'Player twos choice is: {gesture2}')
+        print(f'Player Twos choice is: {gesture2}')
         if gesture1 == gesture2:
+            print('\n')
             continue
         elif gesture1 == 'Rock' and (gesture2 == 'Scissors' or gesture2 == 'Lizard' ):
             player1_counter += 1
+            print(f'{player_1.name} wins this round!\n')
         elif gesture1 == 'Scissors' and (gesture2 == 'Paper' or gesture2 == 'Lizard'):
             player1_counter += 1
+            print(f'{player_1.name} wins this round!\n')
         elif gesture1 == 'Paper' and (gesture2 == 'Rock' or gesture2 == 'Spock'):
             player1_counter +=1 
+            print(f'{player_1.name} wins this round!\n')
         elif gesture1 == 'Lizard' and (gesture2 == 'Spock' or gesture2 == 'Paper'):
             player1_counter +=1
+            print(f'{player_1.name} wins this round!\n')
         elif gesture1 == 'Spock' and (gesture2 == 'Rock' or gesture2 == 'Scissors'):
             player1_counter +=1 
+            print(f'{player_1.name} wins this round!\n')
         else:
             player2_counter +=1
+            print(f'{player_2.name} wins this round!\n')
 else:
     player_2 = Human(input("Input Player 2 name here: "))
+    print('\n')
     while player1_counter < 2 and player2_counter < 2:
         print("1 for Rock, 2 for Scissors, 3 for Paper, 4 for Lizard, 5 for Spock")
         gesture1 = player_1.choose_gesture()
@@ -41,19 +50,26 @@ else:
         print(f'{player_1.name}s choice is: {gesture1}')
         print(f'{player_2.name}s choice is: {gesture2}')
         if gesture1 == gesture2:
+            print('\n')
             continue
         elif gesture1 == 'Rock' and (gesture2 == 'Scissors' or gesture2 == 'Lizard' ):
             player1_counter += 1
+            print(f'{player_1.name} wins this round!\n')
         elif gesture1 == 'Scissors' and (gesture2 == 'Paper' or gesture2 == 'Lizard'):
             player1_counter += 1
+            print(f'{player_1.name} wins this round!\n')
         elif gesture1 == 'Paper' and (gesture2 == 'Rock' or gesture2 == 'Spock'):
             player1_counter +=1 
+            print(f'{player_1.name} wins this round!\n')
         elif gesture1 == 'Lizard' and (gesture2 == 'Spock' or gesture2 == 'Paper'):
             player1_counter +=1
+            print(f'{player_1.name} wins this round!\n')
         elif gesture1 == 'Spock' and (gesture2 == 'Rock' or gesture2 == 'Scissors'):
             player1_counter +=1 
+            print(f'{player_1.name} wins this round!\n')
         else:
             player2_counter +=1
+            print(f'{player_2.name} wins this round!\n')
     
 
 if player1_counter == 2:
--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player:
 
     def choose_gesture(self):
         while True:
-            result = int(input(f" {self.name} Please select a gesture: ")) - 1
+            result = int(input(f"{self.name} - Please select a gesture: ")) - 1
             # result = input(f" {self.name} Please select a gesture: ")
             if result < 0 or result > 4:
                 print("Invalid number. Please input again.")
