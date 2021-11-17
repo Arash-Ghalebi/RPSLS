0cd3b208688b60be185734abd3b38ae61d50fabf
Arash Ghalebi <aghalebi33@gmail.com>
2021-11-17 11:36:16 -0500

    Added a password function so that, in a two player game, player 1 can input a gesture without the other player seeing what they picked.

--- human.py
+++ human.py
@@ -1,6 +1,13 @@
 from player import Player
+import getpass
 
 
 class Human(Player):
-    pass
-           
+    def choose_gesture_secret(self):
+        while True:
+            result = int(getpass.getpass(f"{self.name} - Please select a gesture: ")) - 1
+            # result = input(f" {self.name} Please select a gesture: ")
+            if result < 0 or result > 4:
+                print("Invalid number. Please input again.")
+                continue
+            return self.gestures[result]
